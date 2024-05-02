4c2552305bed1dfadd68b06ae817e5378f30c2e6
VM-Gabriel Mesquita <devgabrielmesquita@gmail.com>
2024-05-02 10:41:13 -0300

    s

--- __init__.py
+++ __init__.py
@@ -117,5 +117,5 @@ def optimize_text(text):
         return text
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #serve(app, host='0.0.0.0', port=5000)
+    #app.run(debug=True)
+    serve(app, host='0.0.0.0', port=5000)
