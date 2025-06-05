b0891aaca344788272ded6698988bb65a8fb4e24
VM-Gabriel Mesquita <devgabrielmesquita@gmail.com>
2025-06-05 17:33:12 -0300

    Corrige Docker e Flask para rodar no container

--- app/main.py
+++ app/main.py
@@ -338,7 +338,7 @@ def optimize_text(text):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 4567))
-    app.run(debug=True, port=port)
+    #app.run(debug=True, host="0.0.0.0", port=port)
     print(f"Server is running on port {port}")
-    #serve(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port)
 
